feaa00e43514f38ce925a1ed31601fa4f2400f40
Yerson Solano <yerson.solano@gmail.com>
2021-02-13 20:41:25 -0800

    initial commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import './search-box.style.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className='search'
+    type='search'
+    placeholder={placeholder}
+    onChange={handleChange}
+  />
+);
